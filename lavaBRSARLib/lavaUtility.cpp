52cd1e6b0ee453da4e09f7078740969a9785e3b5
QuickLava <61454308+QuickLava@users.noreply.github.com>
2022-08-01 19:13:36 -0700

    Separated Byte Manip Stuff Into lavaBytes

--- lavaBRSARLib/lavaUtility.cpp
+++ lavaBRSARLib/lavaUtility.cpp
@@ -66,26 +66,26 @@ namespace lava
 		{
 			switch (numberIn % 10)
 			{
-				case 1:
-				{
-					result += "st";
-					break;
-				}
-				case 2:
-				{
-					result += "nd";
-					break;
-				}
-				case 3:
-				{
-					result += "rd";
-					break;
-				}
-				default:
-				{
-					result += "th";
-					break;
-				}
+			case 1:
+			{
+				result += "st";
+				break;
+			}
+			case 2:
+			{
+				result += "nd";
+				break;
+			}
+			case 3:
+			{
+				result += "rd";
+				break;
+			}
+			default:
+			{
+				result += "th";
+				break;
+			}
 			}
 		}
 		return result;
@@ -148,29 +148,4 @@ namespace lava
 		return temp;
 	}
 
-	std::vector<unsigned char> streamContentsToVec(std::istream& streamIn)
-	{
-		std::vector<unsigned char> result;
-
-		std::streampos currPos = streamIn.tellg();
-		streamIn.seekg(0, std::ios::end);
-		unsigned long streamLength = streamIn.tellg();
-		streamIn.seekg(0, std::ios::beg);
-		result.resize(streamLength, 0x00);
-		streamIn.read((char*)result.data(), result.size());
-		streamIn.seekg(currPos);
-
-		return result;
-	}
-
-	template<>
-	bool writeFundamentalToBuffer<float>(float objectIn, unsigned char* destinationBuffer, endType endianIn)
-	{
-		return writeFundamentalToBuffer<unsigned long>(*(unsigned long*)&objectIn, destinationBuffer, endianIn);
-	}
-	template<>
-	bool writeFundamentalToBuffer<double>(double objectIn, unsigned char* destinationBuffer, endType endianIn)
-	{
-		return writeFundamentalToBuffer<unsigned long long>(*(unsigned long long*) & objectIn, destinationBuffer, endianIn);
-	}
 }
